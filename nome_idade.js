c5d4f5856c243d65f0b2667497ca64583cade996
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-02-23 19:32:35 -0300

    Add files via upload

---
+++ nome_idade.js
@@ -0,0 +1,7 @@
+let nome = "suzana";
+let idade = 17;
+let inteiro = 3;
+let altura = 1.68;
+let peso = 54;
+console.log (`meu nome é ${nome} e minha idade é ${idade} e minha altura é ${altura} e meu peso é ${peso}`);
+console.log (`somando ${idade + inteiro}`);
