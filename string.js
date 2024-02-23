6f69e710a6db3bb28e6b888e26ab022471897cd5
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-02-23 19:31:41 -0300

    Add files via upload

---
+++ string.js
@@ -0,0 +1,13 @@
+string.js
+
+let nome = "suzana";
+let endereco = 'marechal rondon';
+let cachorro1 = 'cristal';
+let cachorro2 = 'meg';
+let cachorro3 = 'naruto';
+
+console.log(`o nome do cachorro é ${cachorro1}`);
+console.log (`o nome do cachorro é ${cachorro2}`); 
+console.log (`o nome do cachorro é ${cachorro3}`);
+console.log (`o nome da aluna é ${nome}`);
+console.log (`o endereço da aluna é ${endereco}`);
