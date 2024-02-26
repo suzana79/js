2cea2573ac73b5596aaabfe01a8147347561a5b2
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-02-26 11:38:50 -0300

    Add files via upload

---
+++ trab.js
@@ -0,0 +1,11 @@
+let cachorro = {
+    nome: 'cristal',
+    raca: 'vira-lata',
+    porte: 'pequeno',
+
+}
+console.log(cachorro)
+
+console.log(cachorro.nome)
+console.log(cachorro.raca)
+console.log(cachorro.porte)
