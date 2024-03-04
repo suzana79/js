2a3bf76b52a4839eeb3338b94da487a5029e2fa8
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-03-04 19:24:47 -0300

    Add files via upload

---
+++ positivo.js
@@ -0,0 +1,11 @@
+numero = 14
+
+if(numero == 0) {
+    console.log('numero neutro')
+} else if (numero > 0) {
+    console.log('numero positivo')
+} else if (numero < 0) {
+    console.log('numero negativo')
+} else {
+    console.log('verifique os dados')
+}
