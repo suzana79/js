6ae5d24980ea27feb93df590bdbbc6ba72065768
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-03-04 19:34:59 -0300

    Add files via upload

---
+++ calculo.js
@@ -0,0 +1,32 @@
+let soma1 = 5.000 + 5.000;
+let soma2 = 10.000 + 5.000;
+let soma3 = 15.000 + 0.350;
+
+console.log(soma3.toFixed(3))
+
+let subtracao1 = -50 -50;
+let subtracao2 = -100 -100;
+let subtracao3 = 200 -200;
+let subtracao4 = -400 -100;
+let subtracao5 = -500 -67;
+
+console.log(subtracao5)
+
+let multiplicacao1 = 490 * 2;
+let multiplicacao2 = 980 * 1;
+
+console.log(multiplicacao2)
+
+let divisaol = 567/3;
+let divisao2 = 189/3;
+let divisao3 = 63/3;
+
+console.log(divisao3)
+
+let restoDivisaol = 21000%9000;
+let restoDivisao2 = 3000%2000;
+let restoDivisao3 = 1000%400;
+let restoDivisao4 = 200%72;
+
+console.log(restoDivisao4)
+	
